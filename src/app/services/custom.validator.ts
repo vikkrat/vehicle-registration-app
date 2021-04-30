5d89534298dedfa3f11a818dde759fe9c0715f06
Viktoriia <poshta.victoria@gmail.com>
2021-04-30 17:34:29 +0300

    added custom validator

---
+++ src/app/services/custom.validator.ts
@@ -0,0 +1,59 @@
+import { CarOwnersService } from './car-owners-service';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class CustomValidator {
+  static registrationNumberValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    if (!control?.value.match('^[A-Z]{2}[0-9]{4}[A-Z]{2}$')) {
+      return { invalidRegistrationNumber: true };
+    }
+    return null;
+  }
+
+  static yearValidator(minYear: number): ValidatorFn {
+    return function (control: AbstractControl): ValidationErrors | null {
+      const currentYear = new Date().getFullYear();
+      const year = control.value;
+      if (
+        year.length != 4 ||
+        parseInt(year) < minYear ||
+        parseInt(year) > currentYear
+      ) {
+        return { invalidYear: true };
+      }
+      return null;
+    };
+  }
+
+  static carMinAmount(minCars: number): ValidatorFn {
+    return function (control: AbstractControl): ValidationErrors | null {
+      const carsAmount = control.value.length;
+      if (carsAmount < minCars) {
+        return { invalidCarAmount: true };
+      }
+      return null;
+    };
+  }
+
+  static carExistValidator(
+    carOwnersService: CarOwnersService,
+    id: string | null
+  ): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      const existingNumbers$ = carOwnersService
+        .checkExistRegistrationNumber(control.value, id)
+        .pipe(
+          map((result: boolean) => (!result ? null : { invalidCarExist: true }))
+        );
+      return existingNumbers$;
+    };
+  }
+}
